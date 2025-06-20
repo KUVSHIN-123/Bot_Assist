f1e117534566e01da242c2d25d0de3208a27aa42
kuvshin <kirikgorbacev@gmail.com>
2025-06-20 22:39:01 +0300

    0.0.3

--- Bot_Assist/Head/bot_loader.py
+++ Bot_Assist/Head/bot_loader.py
@@ -1,7 +1,6 @@
 from aiogram import Bot,Dispatcher
-from dotenv import load_dotenv
-import os
+from Bot_Assist.TOKEN import *
 
-load_dotenv()
-bot = Bot(token=os.getenv('BOT_TOKEN'))
+
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
